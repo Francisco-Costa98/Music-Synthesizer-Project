7be35e9198d289257d2666b09232e1d6d299a411
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-11-02 11:33:35 +0000

    starter code

--- Simple1.asm
+++ Simple1.asm
@@ -3,20 +3,19 @@
 rst	code	0x0000	; reset vector
 	goto	start
 	
-int_hi	code	0x0008	; high vector, no low vector
-	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
-	retfie	FAST		; if not then return
-	incf	LATD		; increment PORTD
-	bcf	INTCON,TMR0IF	; clear interrupt flag
-	retfie	FAST		; fast return from interrupt
-
+setup	movlw	0x00
+	movwf	TRISD, ACCESS
+	movwf	TRISE, ACCESS
+	
 main	code
-start	clrf	TRISD		; Set PORTD as all outputs
-	clrf	LATD		; Clear PORTD outputs
-	movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
-	movwf	T0CON		; = 62.5KHz clock rate, approx 1sec rollover
-	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
-	bsf	INTCON,GIE	; Enable all interrupts
-	goto	$		; Sit in infinite loop
+start	clrf	TRISD	
+	movlw	0x45
+	movwf	PORTD, ACCESS
+	movlw	0x00
+	movwf	PORTE, ACCESS
+	movlw	0x01
+	movwf	PORTE, ACCESS
+	goto	start
+	
 
 	end
