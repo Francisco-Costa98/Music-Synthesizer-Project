fbf2c28f60da0d689c85b8991986dfb3e62256b0
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-11-08 12:26:46 +0000

    tried to impliment interupt code from power point to sine wave code

--- Simple1.asm
+++ Simple1.asm
@@ -1,24 +1,41 @@
 	#include p18f87k22.inc
 
-rst	code	0x0000	; reset vector
-	
-
+code
+	org 0x0
 	goto	setup
 	
+	org 0x100		    ; Main code starts here at address 0x100
+
 setup	movlw	0x00
 	movwf	TRISD, ACCESS
 	movwf	TRISE, ACCESS
 	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
 myArray	res 0x80	; Address in RAM for data
+	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
+	movlw	upper(myTable)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(myTable)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(myTable)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	.30		;22 bytes to read
+	movwf 	counter		; our counter register
 	goto	start
 	; ******* My data and where to put it in RAM *
 myTable  db	0x7f, 0x99, 0xb3, 0xca, 0xdd, 0xed, 0xf8, 0xfd, 0xfd, 0xf8, 0xed, 0xdd, 0xca, 0xb3, 0x99, 0x7f, 0x65, 0x4b, 0x34, 0x21, 0x11, 0x06, 0x01, 0x01, 0x06, 0x11, 0x21, 0x34, 0x4b, 0x65
 	constant    counter=0x02   ; Address of counter variable
+	
+
+	
 main	code
 start	clrf	TRISD	
-	call table_read
-	goto	start
+	clrf	LATD		    ; Clear PORTD outputs
+	movlw b'10000001'	    ; Set timer0 to 16-bit, Fosc/4/256
+	movwf	T0CON		    ; = 62.5KHz clock rate, approx 1sec rollover
+	bsf	INTCON,TMR0IE	    ; Enable timer0 interrupt
+	bsf	INTCON,GIE	    ; Enable all interrupts
+	goto $			    ; Sit in infinite loop
 	
 clock_pulse
 	movlw	0x00
@@ -29,14 +46,6 @@ clock_pulse
 	movlw	0x01
 	movwf	PORTE, ACCESS
 	return
-	
-delay	decfsz 0x50 ; decrement until zero
-	bra delay	
-	return
-
-delay2	decfsz 0x51 ; decrement until zero
-	bra delay2	
-	return
 
 clk_delay decfsz 0x53 ; decrement until zero
 	bra clk_delay	
@@ -44,26 +53,20 @@ clk_delay decfsz 0x53 ; decrement until zero
 	
 	return
 	
-table_read lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.30		;22 bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
+int_hi	code 0x0008		; high vector, no low vector
+	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
+	retfie	FAST		; if not then return
+	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, PORTD	; move read data from TABLAT to (FSR0), increment FSR0	
 	call	clock_pulse
-	movlw	0xAA
-	movwf	0x50
-;	movlw	0x01
-;	movwf	0x51
-	call	delay
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
-	return
+	dcfsnz	counter		; count down to zero
+	call	counter_reset
+	bcf	INTCON,TMR0IF	; clear interrupt flag
+	retfie	FAST		; fast return from interrupt
 	
+counter_reset
+	movlw	.30		;22 bytes to read
+	movwf 	counter		; our counter register
+	return
 	
 	end
