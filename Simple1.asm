fa4e8796215574e49250e85cb103c1b82422b686
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-11-12 15:24:25 +0000

    working stop start sound code

--- Simple1.asm
+++ Simple1.asm
@@ -1,7 +1,10 @@
 	#include p18f87k22.inc
 	
-	extern  setup_keypad, keypad_start, khigh, klow
+	extern  setup_keypad, keypad_start, khigh, klow, test
 
+acs0    udata_acs
+	counter res 1
+ 
 rst	code	0    ; reset vector
 	goto	setup
 	
@@ -17,21 +20,10 @@ setup	call	setup_keypad
 	movwf	CCPR4H
 	movlw	0xa9
 	movwf	CCPR4L
-myArray	res 0x80	; Address in RAM for data
-	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.30		;22 bytes to read
-	movwf 	counter		; our counter register
+	call	counter_reset
 	goto	start
 	; ******* My data and where to put it in RAM *
-myTable  db	0x7f, 0x99, 0xb3, 0xca, 0xdd, 0xed, 0xf8, 0xfd, 0xfd, 0xf8, 0xed, 0xdd, 0xca, 0xb3, 0x99, 0x7f, 0x65, 0x4b, 0x34, 0x21, 0x11, 0x06, 0x01, 0x01, 0x06, 0x11, 0x21, 0x34, 0x4b, 0x65
-	constant    counter=0x05   ; Address of counter variable
-	
+myTable  db	0x7f, 0x99, 0xb3, 0xca, 0xdd, 0xed, 0xf8, 0xfd, 0xfd, 0xf8, 0xed, 0xdd, 0xca, 0xb3, 0x99, 0x7f, 0x65, 0x4b, 0x34, 0x21, 0x11, 0x06, 0x01, 0x01, 0x06, 0x11, 0x21, 0x34, 0x4b, 0x65	
 
 start	clrf	TRISD	
 	clrf	LATD		    ; Clear PORTD outputs
@@ -49,13 +41,11 @@ start	clrf	TRISD
 	goto $			    ; Sit in infinite loop
 	
 clock_pulse
-	movlw	0x00
-	movwf	PORTC, ACCESS
+	bcf	PORTC, 0
 	movlw	0x0A
 	movwf	0x53
 	call	clk_delay
-	movlw	0x01
-	movwf	PORTC, ACCESS
+	bsf	PORTC, 0
 	return
 
 clk_delay decfsz 0x53 ; decrement until zero
@@ -64,21 +54,17 @@ clk_delay decfsz 0x53 ; decrement until zero
 
 	
 int_hi	code 0x0008		; high vector, no low vector
-	btfss	PIR4,CCP4IF	; check that this is timer0 interrupt
-	retfie	FAST		; if not then return
+	btfss	PIR4,CCP4IF	; check that this is timer1 interrupt
+	retfie	1		; if not then return
 	call	keypad_start
 	movff	khigh, CCPR4H
 	movff	klow, CCPR4L
-	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, PORTD	; move read data from TABLAT to (FSR0), increment FSR0	
-	call	clock_pulse
-	dcfsnz	counter		; count down to zero
-	call	counter_reset
+	tstfsz	test, 0
+	call	read
 	bcf	PIR4,CCP4IF	; clear interrupt flag
 	retfie	FAST		; fast return from interrupt
 	
 counter_reset
-	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
 	movlw	upper(myTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
@@ -87,6 +73,13 @@ counter_reset
 	movwf	TBLPTRL		; load low byte to TBLPTRL
 	movlw	.30		;22 bytes to read
 	movwf 	counter		; our counter register
+	return
+	
+read	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, PORTD	; move read data from TABLAT to (FSR0), increment FSR0	
+	call	clock_pulse
+	dcfsnz	counter		; count down to zero
+	call	counter_reset
 	return
 	
 	end
