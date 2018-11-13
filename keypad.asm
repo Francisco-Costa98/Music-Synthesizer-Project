a7934e8d6ccf464a3f2cb4471d29a9a313dd7837
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-11-13 09:45:38 +0000

    neatened up keypad code and commented

--- keypad.asm
+++ keypad.asm
@@ -2,7 +2,7 @@
 	
 	global setup_keypad, keypad_start, khigh, klow, test
 	
-acs0    udata_acs
+acs0    udata_acs ;reserves space for variables used
 test	res 1
 khigh	res 1
 klow	res 1
@@ -10,50 +10,48 @@ testreg1 res 1
 testreg	res 1
 keypad_delay	res 1
 
-keypad    code
+keypad    code ;main code
 
 
-setup_keypad	
+setup_keypad	; routine to set up keypad
 	banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
-	bsf	PADCFG1, REPU, BANKED ; PortE pull-ups on
+	bsf	PADCFG1, RJPU, BANKED ; PortE pull-ups on
 	movlb	0x00 ; set BSR back to Bank 0
-	setf	TRISE, ACCESS ; Tri-state PortE	
-	clrf	LATE
-	clrf	LATH
-	movlw	0x0F
-	movwf	TRISE, ACCESS
-	movlw	0x00
+	setf	TRISJ, ACCESS ; Tri-state PortE	
+	clrf	LATJ	;clears latch J
+	clrf	LATH	;clears latch H
+	movlw	0x00	;sets portH to output port
 	movwf	TRISH, ACCESS
-	movlw	0x0A
+	movlw	0x0A	; values moved into delay register
 	movwf	keypad_delay
-	movlw	0x00		    ; initialises ccp4 module with timer1 for compare and timer2 for pwm
+	movlw	0x00	;initialises value for khigh
 	movwf	khigh
-	movlw	0xa9
+	movlw	0xa9	;initialises value for klow
 	movwf	klow
 	return
 
 	
 	
-keypad_start	
-	movlw	0x01
-	movwf	test
-	movlw	0x0F
-	movwf	TRISE, ACCESS
-	movlw	0x0A
-	movwf	keypad_delay
-	call	delay
-	movff	PORTE, testreg1
+keypad_start	    ;routine to read keypad
+	movlw	0x01	; initiliases test value to se if keypad is pressed
+	movwf	test	;moves into the test register
+	movlw	0x0F	; lights up one side of portJ (keypad port) for logic operations
+	movwf	TRISJ, ACCESS	; moves value to port j
+	movlw	0x0A	;delay keypad register set up
+	movwf	keypad_delay	;moves value to delay register
+	call	delay	;delays keypad
+	movff	PORTJ, testreg1	;moves value presed in port J to test register one
 	movlw	0x00
-	addwf	testreg1, 0, 0
-	sublw	0x0F
-	movwf	testreg1
+	addwf	testreg1, 0, 0	; moves test register value to w register
+	sublw	0x0F	; subtracts value from 0F to see which bit its pressed
+	movwf	testreg1    ;moves value of pressed bit to test register 1
 	
-	movlw	0xF0
-	movwf	TRISE, ACCESS
-	movlw	0x0A
-	movwf	keypad_delay
-	call	delay
-	movff	PORTE, testreg
+	movlw	0xF0	    ; lights up other side of the keypad port (port J)
+	movwf	TRISJ, ACCESS	;moves value to portj
+	movlw	0x0A	;delay keypad register set up
+	movwf	keypad_delay	;moves value to delay register
+	call	delay	;delays keypad
+	movff	PORTJ, testreg	;moves value in port j to test register
 	movlw	0x00
 	addwf	testreg, 0, 0
 	sublw	0xF0
@@ -159,16 +157,16 @@ test9	movlw	.68
 testA	movlw	.129
 	cpfseq	testreg
 	bra	testB
-	movlw	0x0A
+	movlw	0x00
 	movwf	khigh
-	movlw	0x41 
+	movlw	0x63 
 	movwf	klow
 	call	Write
 	goto	finish
 testB	movlw	.132
 	cpfseq	testreg
 	bra	testC
-	movlw	0x0B
+	movlw	0x00
 	movwf	khigh
 	movlw	0x42
 	movwf	klow
@@ -177,36 +175,36 @@ testB	movlw	.132
 testC	movlw	.136
 	cpfseq	testreg
 	bra	testD
-	movlw	0x0C
+	movlw	0x00
 	movwf	khigh
-	movlw	0x43
+	movlw	0x21
 	movwf	klow
 	call	Write
 	goto	finish
 testD	movlw	.72
 	cpfseq	testreg
 	bra	testE
-	movlw	0x0D
+	movlw	0x0
 	movwf	khigh
-	movlw	0x44
+	movlw	0x10
 	movwf	klow
 	call	Write
 	goto	finish
 testE	movlw	.40
 	cpfseq	testreg
 	bra	testF
-	movlw	0x0E
+	movlw	0x00
 	movwf	khigh
-	movlw	0x45
+	movlw	0x0A
 	movwf	klow
 	call	Write
 	goto	finish
 testF	movlw	.24
 	cpfseq	testreg
 	bra	finish2
-	movlw	0x0F
+	movlw	0x00
 	movwf	khigh
-	movlw	0x46
+	movlw	0x01
 	movwf	klow
 	call	Write
 	goto	finish
@@ -236,21 +234,7 @@ Write	movlw	0x0A
 	movwf	keypad_delay
 	call	delay
 	movff	klow, PORTH
-	
 	return
-	
-phat_delay	decfsz 0x20 ; decrement until zero
-	bra phat_delay
-	call delay2	
-	return
-	
-delay2	decfsz 0x21 ; decrement until zero	
-	bra phat_delay
-	call delay3
-	return
-		
-delay3	decfsz 0x22 ; decrement until zero	
-	bra phat_delay
-	return
+
 
     end
