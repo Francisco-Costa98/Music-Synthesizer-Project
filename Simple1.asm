638f8977f2f55a05ce73a5b9d34a8ebebf385861
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-11-16 12:23:45 +0000

    COMMENTED TRYING TO IMPLEMENT SONG

--- Simple1.asm
+++ Simple1.asm
@@ -3,12 +3,13 @@
 	extern  setup_keypad, keypad_start, khigh, klow, test, LCD_Setup, a_test, chord_test
 
 acs0    udata_acs		    ; reserves space for variables used
-	counter res 1		    ; reserves one bite for counter 
-	delayreg res 1		    ; reserve one byte for delay register
-	thic1 res 1		    ; reserves byte for cascading delay register
-	thic2 res 1		    ; reserves byte for cascading delay register
-	thic3 res 1		    ; reserves byte for cascading delay register
-	song_counter res 1	    ; reserves byte for song counter
+counter res 1		    ; reserves one bite for counter 
+delayreg res 1		    ; reserve one byte for delay register
+delayadrs1 res 1		    ; reserves byte for cascading delay register
+delayadrs2 res 1		    ; reserves byte for cascading delay register
+delayadrs3 res 1		    ; reserves byte for cascading delay register
+song_counter res 1	    ; reserves byte for song counter
+fullsong    res 1
  
 rst	code	0		    ; reset vector
 	goto	setup		    ; goes to code setup
@@ -22,15 +23,21 @@ setup	call	setup_keypad	    ; sets up keypad
 	movwf	TRISC, ACCESS	    ; sets port c to output
 	bcf	EECON1, CFGS	    ; point to Flash program memory  
 	bsf	EECON1, EEPGD	    ; access Flash program memory
+	movlw	0xF0		    ; gives 2 full loops of each note for song play 
+	movwf	fullsong	    ; stores it in the count checker
 	call	counter_reset	    ; sets up table and counter for reading data
 	goto	start		    ; goes to start of code
 	; ******* My data **
 myTable  db	0x7f, 0x99, 0xb3, 0xca, 0xdd, 0xed, 0xf8, 0xfd, 0xfd, 0xf8, 0xed, 0xdd, 0xca, 0xb3, 0x99, 0x7f, 0x65, 0x4b, 0x34, 0x21, 0x11, 0x06, 0x01, 0x01, 0x06, 0x11, 0x21, 0x34, 0x4b, 0x65	
 songTable db	0x68, 0x41, 0x99, 0xFF, 0xDE, 0x67
 chordTable db	0xfc, 0xba, 0x7b, 0x41, 0x1d, 0x18, 0x2f, 0x58, 0x82, 0xa0, 0xaa, 0xa3, 0x92, 0x82, 0x7e, 0x87, 0x98, 0xa6, 0xa8, 0x96, 0x73, 0x47, 0x24, 0x17, 0x28, 0x56, 0x94, 0xcf, 0xf5
- 
+	constant 	song1=0x400
+	
 start	clrf	LATC		    ; clears port c outputs	
 	clrf	LATD		    ; Clear PORTD outputs
+	call	song_setup
+	call	fsrload
+	;call	counter_setup
 	movlw b'00110001'	    ; Set timer1 to 16-bit, Fosc/1:8
 	movwf	T1CON		    ; = 16MHz clock rate, approx 1sec rollover
 	banksel CCPTMRS1	    ; CCPTMRS1 is in banked ram
@@ -44,34 +51,22 @@ start	clrf	LATC		    ; clears port c outputs
 	bsf	INTCON,GIE	    ; Enable all interrupts
 	goto $			    ; Sit in infinite loop
 	
-clock_pulse			    ; clock pulse routine to make DAC read in values
-	bcf	PORTC, 0	    ; clears clock enable bit
-	movlw	0x0A		    ; sets up a delay
-	movwf	delayreg	    ; moves delayvalue to register 0x53
-	call	clk_delay	    ; calls delay
-	bsf	PORTC, 0	    ; sets clock enable bit
-	return
-
-clk_delay			    ; clock delay routine
-	decfsz delayreg		    ; decrement register 0x53 until zero
-	bra clk_delay		    ; loops until register is zero
-	return
-
-	
 int_hi	code 0x0008		    ; high vector, no low vector
 	btfss	PIR4,CCP4IF	    ; check that this is timer1 interrupt
 	retfie	FAST		    ; if not then return
 	call	keypad_start	    ; calls keypad start routine
 	movff	khigh, CCPR4H	    ; from keypad start routine moves value of khigh to CCP register
 	movff	klow, CCPR4L	    ; from keypad start routine moves value of klow to CCP register
-	tstfsz	a_test, 0	    ; checks if c is pressed on the keypad, if nothing is pressed no values are read
-	call	play_song	    ; calls song sub-routine if c is pressed
+	tstfsz	a_test, 0	    ; checks if a is pressed on the keypad, if nothing is pressed no values are read
+	call	songloop	    ; calls song sub-routine if c is pressed
 	tstfsz	chord_test, 0	    ; checks if chord key is pressed on the keypad, if nothing is pressed no values are read
 	call	read_chord	    ; plays chord if certain buttons are pressed
 	tstfsz	test, 0		    ; checks if nothing is pressed on the keypad, if nothing is pressed no values are read
 	call	read		    ; reads values from table
 	bcf	PIR4,CCP4IF	    ; clear interrupt flag
 	retfie	FAST		    ; fast return from interrupt
+	
+; ******** PLAYS NOTES ******
 	
 counter_reset			    ; counter reset routine to reset the counter when it goes to zero 
 	movlw	upper(myTable)	    ; address of data in PM
@@ -90,53 +85,9 @@ read				    ; read routine to  read values from table
 	call	clock_pulse	    ; calls clock pulse to read in values
 	dcfsnz	counter		    ; count down to zero
 	call	counter_reset	    ; if counte is zero, the counter is reset
-	return	
-	
-play_song			    ; subroutine to play song
-	call song_setup		    ; sets up song
-	call read_song		    ; reads note from song
-	movlw 0x10		    ; moves value to delay register
-	movwf thic1		    ; initialises delay register
-	movwf thic2		    ; initialises delay register
-	movwf thic3		    ; initialises delay register
-	call thicc_delay	    ; calls delay
-	call counter_reset	    ; resets counter
-	return
-	
-	
-song_setup
-	movlw	upper(songTable)    ; address of data in PM
-	movwf	TBLPTRU		    ; load upper bits to TBLPTRU
-	movlw	high(songTable)	    ; address of data in PM
-	movwf	TBLPTRH		    ; load high byte to TBLPTRH
-	movlw	low(songTable)	    ; address of data in PM
-	movwf	TBLPTRL		    ; load low byte to TBLPTRL
-	movlw	.6		    ; 30 bytes to read
-	movwf 	counter		    ; our counter register
 	return
 
-read_song			    ; read routine to read values from table
-	tblrd*+			    ; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, CCPR4L	    ; move read data from TABLAT to (FSR0), increment FSR0	
-	call	clock_pulse	    ; calls clock pulse to read in values
-	dcfsnz	song_counter	    ; count down to zero
-	call	song_setup	    ; if counte is zero, the counter is reset
-	return	
-	
-thicc_delay	
-	decfsz thic1		    ; decrement until zero
-	bra thicc_delay		    ; branch back to delay
-	call delay2		    ; calls next delay
-	return
-	
-delay2	decfsz thic2		    ; decrement until zero	
-	bra delay2		    ; branch back to delay
-	call delay3		    ; calls next delay
-	return
-		
-delay3	decfsz thic3		    ; decrement until zero	
-	bra delay3		    ; branch back to delay
-	return
+; ******* PLAYS CHORDS **********	
 	
 chord_setup			    ; counter reset routine to reset the counter when it goes to zero 
 	movlw	upper(chordTable)   ; address of data in PM
@@ -155,6 +106,90 @@ read_chord
 	call	clock_pulse	    ; calls clock pulse to read in values
 	dcfsnz	counter		    ; count down to zero
 	call	chord_setup	    ; if count is zero, the counter is reset
+	return	
+	
+; ********* PLAYS SONGS *************	
+	
+songloop
+	dcfsnz	fullsong			     
+	call	play_song
+	return
+	
+play_song			    ; subroutine to play song
+	movlw	0xF0		    ; gives 2 full loops of each note for song play 
+	movwf	fullsong	    ; stores it in the count checker
+	call	read_song	    ; calls next delay
+	;movwf delayadrs2	    ; initialises delay register
+	;movwf delayadrs3	    ; initialises delay register
+	;call big_delay		    ; calls delay
+	;call counter_reset	    ; resets counter
+	return
+	
+
+read_song			    ; read routine to read values from table
+	movlw	0x00
+	movwf	CCPR4H
+	movff	POSTINC0, CCPR4L	    ; move read data from TABLAT to (FSR0), increment FSR0	
+	;call	clock_pulse	    ; calls clock pulse to read in values
+	dcfsnz	song_counter	    ; count down to zero
+	call	song_counterreset	    ; if counte is zero, the counter is reset
+	return	
+		
+song_setup 	
+	lfsr	FSR0, song1	; Load FSR0 with address in RAM	
+	movlw	upper(songTable); address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(songTable)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(songTable)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	.5		; 5 notes to read
+	movwf 	song_counter	; our counter register
+	return
+	
+fsrload 
+	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0; move read data from TABLAT to (FSR0), increment FSR0	
+	decfsz	song_counter	; count down to zero
+	bra	fsrload		; keep going until finished
+	call	song_counterreset
+	return
+	
+song_counterreset
+	movlw	.5		; 5 notes to read
+	movwf 	song_counter	; our counter register
+	lfsr	FSR0, song1
+	return
+		
+
+; ********* DELAYS AND CLOCK PULSES********
+	
+big_delay	
+	decfsz delayadrs1	    ; decrement until zero
+	bra big_delay		    ; branch back to delay
+	call delay2		    ; calls next delay
+	return	
+	
+delay2	decfsz delayadrs2	    ; decrement until zero	
+	bra delay2		    ; branch back to delay
+	call delay3		    ; calls next delay
+	return
+		
+delay3	decfsz delayadrs3	    ; decrement until zero	
+	bra delay3		    ; branch back to delay
+	return
+	
+clock_pulse			    ; clock pulse routine to make DAC read in values
+	bcf	PORTC, 0	    ; clears clock enable bit
+	movlw	0x0A		    ; sets up a delay
+	movwf	delayreg	    ; moves delayvalue to register 0x53
+	call	clk_delay	    ; calls delay
+	bsf	PORTC, 0	    ; sets clock enable bit
+	return
+
+clk_delay			    ; clock delay routine
+	decfsz delayreg		    ; decrement register 0x53 until zero
+	bra clk_delay		    ; loops until register is zero
 	return
 	
 	end
